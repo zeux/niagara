87fcf484a00c718a6d54d43efe18ac55cec323f7
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2018-11-20 08:24:14 -0800

    Simplify sphere projection even further
    All scalar factors that are shared between two components of min/max x/y
    are redundant since we divide anyway so there's no need to compute
    length(cx) and divide by it as it's a scalar factor on the result of
    matrix multiplication.
    
    Also reorder min/max y in clip space computation and more cleanly
    separate it from UV space (where we flip the Y coordinate so min/max hav
    to flip)

--- src/shaders/drawcull.comp.glsl
+++ src/shaders/drawcull.comp.glsl
@@ -50,16 +50,17 @@ bool projectSphere(vec3 C, float r, float znear, float P00, float P11, out vec4 
 		return false;
 
 	vec2 cx = -C.xz;
-	vec2 vx = vec2(sqrt(dot(cx, cx) - r * r), r) / length(cx);
+	vec2 vx = vec2(sqrt(dot(cx, cx) - r * r), r);
 	vec2 minx = mat2(vx.x, vx.y, -vx.y, vx.x) * cx;
 	vec2 maxx = mat2(vx.x, -vx.y, vx.y, vx.x) * cx;
 
 	vec2 cy = -C.yz;
-	vec2 vy = vec2(sqrt(dot(cy, cy) - r * r), r) / length(cy);
-	vec2 miny = mat2(vy.x, -vy.y, vy.y, vy.x) * cy;
-	vec2 maxy = mat2(vy.x, vy.y, -vy.y, vy.x) * cy;
+	vec2 vy = vec2(sqrt(dot(cy, cy) - r * r), r);
+	vec2 miny = mat2(vy.x, vy.y, -vy.y, vy.x) * cy;
+	vec2 maxy = mat2(vy.x, -vy.y, vy.y, vy.x) * cy;
 
-	aabb = vec4(minx.x / minx.y * P00, miny.x / miny.y * P11, maxx.x / maxx.y * P00, maxy.x / maxy.y * P11) * vec4(0.5f, -0.5f, 0.5f, -0.5f) + vec4(0.5f);
+	aabb = vec4(minx.x / minx.y * P00, miny.x / miny.y * P11, maxx.x / maxx.y * P00, maxy.x / maxy.y * P11);
+	aabb = aabb.xwzy * vec4(0.5f, -0.5f, 0.5f, -0.5f) + vec4(0.5f); // clip space -> uv space
 
 	return true;
 }

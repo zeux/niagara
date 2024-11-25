7a42c2d53bf1a81b935ef3bb253be9ba169fdea5
feelamee <106318766+feelamee@users.noreply.github.com>
2024-11-25 06:08:14 +0000

    Add missing <cstring> include (#39)

--- src/scene.cpp
+++ src/scene.cpp
@@ -11,6 +11,7 @@
 
 #include <algorithm>
 #include <memory>
+#include <cstring>
 
 static size_t appendMeshlets(Geometry& result, const std::vector<Vertex>& vertices, const std::vector<uint32_t>& indices, bool fast = false)
 {

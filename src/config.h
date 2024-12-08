82cff6fabd3caee84d0204b64aba4fac442d6dc9
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-12-08 14:49:22 -0800

    Add CONFIG_RELVAL to enable validation in Release
    This allows to mostly work in Release; we also don't abort on validation
    errors in that configuration which might be a good thing for sync
    errors.

--- src/config.h
+++ src/config.h
@@ -23,6 +23,9 @@
 // Should we enable vertical sync during presentation? Worth setting to 0 when doing perf profiling to avoid GPU downclock during idle
 #define CONFIG_VSYNC 1
 
+// Should we enable validation layers in release? (they are always enabled in debug)
+#define CONFIG_RELVAL 0
+
 // Should we enable synchronization validation? Worth running with 1 occasionally to check correctness.
 #define CONFIG_SYNCVAL 0
 
--- src/device.cpp
+++ src/device.cpp
@@ -10,6 +10,8 @@
 // Validation is enabled by default in Debug
 #ifndef NDEBUG
 #define KHR_VALIDATION 1
+#else
+#define KHR_VALIDATION CONFIG_RELVAL
 #endif
 
 // Synchronization validation is disabled by default in Debug since it's rather slow

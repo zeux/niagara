31043a7ed7a6e8229804cfc96e2dab1ccc09709c
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2025-08-22 09:29:21 -0700

    Remove stageBarrier in preparation for the stream
    This will be reimplemented on stream :)

--- src/scenert.cpp
+++ src/scenert.cpp
@@ -437,7 +437,11 @@ void buildCBLAS(VkDevice device, const std::vector<Mesh>& meshes, const std::vec
 
 	vkCmdBuildClusterAccelerationStructureIndirectNV(commandBuffer, &clusterMove);
 
-	stageBarrier(commandBuffer, VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_KHR);
+	VkBufferMemoryBarrier2 barrier = bufferBarrier(blasBuffer.buffer,
+	    VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_KHR, VK_ACCESS_ACCELERATION_STRUCTURE_WRITE_BIT_KHR,
+	    VK_PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT_KHR, VK_ACCESS_ACCELERATION_STRUCTURE_READ_BIT_KHR);
+
+	pipelineBarrier(commandBuffer, 0, 1, &barrier, 0, nullptr);
 
 	vkCmdBuildClusterAccelerationStructureIndirectNV(commandBuffer, &accelBuild);
 
